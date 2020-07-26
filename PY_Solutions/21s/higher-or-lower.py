a9aeb41aa518fc75363328260d8939f99d301f3f
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-26 17:00:58 +0100

    h/l finished

--- PY_Solutions/21s/higher-or-lower.py
+++ PY_Solutions/21s/higher-or-lower.py
@@ -7,41 +7,45 @@ import random
 
 repeat = True
 
-# while repeat:
+while repeat:
 
-cards = []
+    cards = []
 
+    def start_game():
+        global repeat
+        create_cards()
+        shuffle_cards()
+        get_user_input()
+        again = input('play again? (y/n): ').lower()
+        if again == 'n':
+            repeat = False
 
-def start_game():
-    create_cards()
-    shuffle_cards()
-    get_user_input()
+    def create_cards():
+        for i in range(0, 2):
+            for x in range(1, 11):
+                cards.append(x)
 
+    def shuffle_cards():
+        random.shuffle(cards)
 
-def create_cards():
-    for i in range(0, 2):
-        for x in range(1, 11):
-            cards.append(x)
-
-
-def shuffle_cards():
-    random.shuffle(cards)
-    print(cards)
-
-
-def get_user_input():
-    correct = 0
-    while len(cards) > 1 and correct != 4:
-        print(cards[0])
-        choice = input('will the next cards be higher or lower? (h/l): ')
-        if choice == 'h' and cards[1] > cards[0]:
-            print('correct, it was: ', cards[1])
-            correct += 1
-        elif choice == 'l' and cards[1] < cards[0]:
-            print('correct, it was: ', cards[1])
-            correct += 1
+    def get_user_input():
+        correct = 0
+        while len(cards) > 1 and correct != 4:
+            print(cards[0])
+            choice = input(
+                'will the next cards be higher or lower? (h/l): ').lower()
+            if choice == 'h' and cards[1] > cards[0]:
+                print('correct, it was: ', cards[1])
+                correct += 1
+            elif choice == 'l' and cards[1] < cards[0]:
+                print('correct, it was: ', cards[1])
+                correct += 1
+            else:
+                print('wrong, it was: ', cards[1])
+            cards.pop(0)
+        if correct != 4:
+            print('you lost, number of correct guesses: ', correct)
         else:
-            print('wrong, it was: ', cards[1])
-        cards.pop(0)
+            print('you win!')
 
-start_game()
+    start_game()
