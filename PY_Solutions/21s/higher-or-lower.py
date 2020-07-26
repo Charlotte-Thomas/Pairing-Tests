12ed82e6a059fe87027a3a70e47e233bf2373dc4
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-26 16:47:39 +0100

    main logic complete

--- PY_Solutions/21s/higher-or-lower.py
+++ PY_Solutions/21s/higher-or-lower.py
@@ -2,7 +2,7 @@ import random
 
 # user given a number from a selection of cards
 # user must guess if the next card will be higher or lower
-# 5 correct guesses = winner
+# 4 correct guesses = winner
 # looses if cards run out
 
 repeat = True
@@ -30,14 +30,18 @@ def shuffle_cards():
 
 
 def get_user_input():
-    while len(cards) > 1:
+    correct = 0
+    while len(cards) > 1 and correct != 4:
         print(cards[0])
-        choice = input('will the nect cards be higher or lower? (h/l): ')
+        choice = input('will the next cards be higher or lower? (h/l): ')
         if choice == 'h' and cards[1] > cards[0]:
-            print('correct')
-            cards.pop(0)
+            print('correct, it was: ', cards[1])
+            correct += 1
+        elif choice == 'l' and cards[1] < cards[0]:
+            print('correct, it was: ', cards[1])
+            correct += 1
         else:
-            print('wrong')
-
+            print('wrong, it was: ', cards[1])
+        cards.pop(0)
 
 start_game()
