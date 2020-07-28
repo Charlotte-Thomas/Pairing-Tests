3510fed39c3d65feb16dee0f18b43ad9962e0ab8
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-28 15:30:08 +0100

    exchange_money func

--- PY_Solutions/21s/fruit-machine.py
+++ PY_Solutions/21s/fruit-machine.py
@@ -4,20 +4,34 @@ import random
 # variables for player and machine money
 
 repeat = True
+player_money = 50
+machine_money = 1000
+
 while repeat:
 
     colours = ['black', 'white', 'green', 'yellow']
 
     def start_game():
-        global repeat
-        slots = random_slots()
-        if check_slots(slots):
-            print('jackpot!')
-        else:
-            print('you loose, try again')
-        choice = input('try again? (y/n): ').lower()
+        if exchange_money():
+            global repeat
+            slots = random_slots()
+            if check_slots(slots):
+                print('jackpot!')
+            else:
+                print('you loose, try again')
+        choice = input(f'player money: {player_money}, try again? (y/n): ').lower()
         if choice == 'n':
             repeat = False
+
+    def exchange_money():
+        global repeat, machine_money, player_money
+        if player_money < 10:
+            print('player does not have enough money to continue')
+            return False
+        else:
+            machine_money += 10
+            player_money -= 10
+            return True
 
     def random_slots():
         slots = random.choices(colours, k=4)
