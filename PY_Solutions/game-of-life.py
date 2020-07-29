66f3d875531dec1145dc5613bba6a1ca36be025b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-29 18:59:56 +0100

    place cells func

--- PY_Solutions/game-of-life.py
+++ PY_Solutions/game-of-life.py
@@ -1,7 +1,10 @@
 
 def start_game():
     grid = create_grid()
+    starting_cells = [[0, 1], [0, 2], [0, 3], [0, 4]]
+    grid = place_cells(grid, starting_cells)
     show_grid(grid, 1)
+
 
 def create_grid():
     grid = []
@@ -12,10 +15,17 @@ def create_grid():
         grid.append(row)
     return grid
 
+
+def place_cells(grid, starting_cells):
+    for cell in starting_cells:
+        grid[cell[0]][cell[1]] = 'x'
+    return grid
+
 def show_grid(grid, wave):
     print('wave:', wave)
     for col in grid:
         print((' ').join(col))
     print('\n')
+
 
 start_game()
